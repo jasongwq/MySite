c8c743f1a3e435af53daedfdc194edae03638fcf
jasongwq <jasongwq@gmail.com>
2015-08-29 07:50:00 +0800

    增加多线程

--- mysite/manage.py
+++ mysite/manage.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
+import socketMethod
 
 if __name__ == "__main__":
+    socketMethod.socketMethod()
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
     from django.core.management import execute_from_command_line
